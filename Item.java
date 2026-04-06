6d1d13f0a0675b32ee4d1995ab3c88310dc2687b
Isabela Pinheiro <bellaolivepit@gmail.com>
2026-04-05 23:01:39 -0300

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,25 @@
+public class Item {
+    private Produto produto;
+    private int quantidade;
+
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorTotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
